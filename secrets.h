f204b113612246ae89bdb482c23259438fe44ceb
Miroslav Remias <miroslavremias@MacBook-Air.local>
2026-02-09 10:23:08 +0100

    Initial commit

---
+++ secrets.h
@@ -0,0 +1,9 @@
+#pragma once
+
+static const char* WIFI_SSID     = "Sunrise_5051702";
+static const char* WIFI_PASS     = "z9AmhnM4zzfdoawc";
+
+static const char* MQTT_SERVER   = "192.168.1.144";
+static const int   MQTT_PORT     = 1883;
+static const char* MQTT_USER     = "mqtt";
+static const char* MQTT_PASS     = "mqtt";
